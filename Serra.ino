f707846ef8c6bc4e51c5c83e9ee81bdc8d717537
Valerio Franzese <valeplay.98@gmail.com>
2020-04-21 13:01:21 +0200

    Update Serra.ino

--- Serra.ino
+++ Serra.ino
@@ -14,15 +14,22 @@
  */
 #define rele 12
 #define sensore_uno A0
+#define sensore_luce A1 //il mio cicuito è invertito, il risultato esatto è il reciproco dell'output del sensore, ho quindi invertito maggiore e minore
+
 byte statorele;
 int umidita=0;
 bool POMPA;
+bool GIORNO;
+int luce;
+
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
 void setup() {
   pinMode(rele, OUTPUT);
   pinMode(sensore_uno, INPUT);
+  pinMode(luce, INPUT);
   Serial.begin(9600);
   lcd.begin();
   lcd.clear();
@@ -34,12 +41,14 @@ void setup() {
 void loop() {
   umidita=analogRead(sensore_uno);
   umidita=map(umidita, 1023, 275, 0 , 100);
-  if (umidita<70) {
-    digitalWrite(rele,LOW);
+  if (umidita<55) {
     statorele=HIGH;
     Serial.print("Umidità:");
     Serial.print(umidita);
     Serial.println("%");
+    stato_giorno();
+    Serial.println(luce);
+    Serial.println(GIORNO);
     stato_pompa();
     infoschermo();
     
@@ -50,6 +59,9 @@ void loop() {
     Serial.print("Umidità:");
     Serial.print(umidita);
     Serial.println("%");
+    stato_giorno();
+    Serial.println(luce);
+    Serial.println(GIORNO);
     stato_pompa();
     infoschermo();
    }
@@ -72,6 +84,8 @@ void infoschermo(){
     lcd.print("OFF");
     }
   }
+
+
 void stato_pompa(){
   if(statorele==HIGH){
     POMPA=true;
@@ -82,4 +96,13 @@ void stato_pompa(){
   
 
 }
-  
+void stato_giorno(){
+  luce=analogRead(sensore_luce);
+  luce=map(luce, 0, 1024, 0, 100);
+  if(luce<=30){ //qui ho invertito maggiore e minore (vedi riga 17);
+    GIORNO=true;
+    }
+    else {
+      GIORNO=false;
+      }
+  }
