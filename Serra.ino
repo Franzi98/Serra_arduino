2a9be15eacab4cbbd037882d6ee72055671069f4
Valerio Franzese <valeplay.98@gmail.com>
2020-04-25 19:01:12 +0200

    update Serra.ino

--- Serra.ino
+++ Serra.ino
@@ -4,19 +4,9 @@
 
 #include <Stepper.h>
 
-/*
- * 
- * 0 ~300 : dry soil ( terreno asciutto )
- * 300~700 : humid soil ( terreno umido )
- * 700~950 : in water ( in acqua )
- *  A0= umidità terreno
-
- */
-#define rele 12
+#define pin_pompa 8
 #define sensore_uno A0
-#define sensore_luce A1 //il mio cicuito è invertito, il risultato esatto è il reciproco dell'output del sensore, ho quindi invertito maggiore e minore
 
-byte statorele;
 int umidita=0;
 bool POMPA;
 bool GIORNO;
@@ -27,10 +17,9 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
 void setup() {
-  pinMode(rele, OUTPUT);
+  pinMode(pin_pompa, OUTPUT);
   pinMode(sensore_uno, INPUT);
-  pinMode(luce, INPUT);
-  Serial.begin(9600);
+  digitalWrite(pin_pompa,LOW);
   lcd.begin();
   lcd.clear();
   lcd.backlight();
@@ -38,33 +27,25 @@ void setup() {
 
 }
 
-void loop() {
-  umidita=analogRead(sensore_uno);
-  umidita=map(umidita, 1023, 275, 0 , 100);
-  stato_giorno();
-  if (umidita<55 || GIORNO=true) {
-    statorele=HIGH,
-    Serial.println(luce);
-    Serial.println(GIORNO);
-    stato_pompa();
-    infoschermo();
+void loop(){
+  leggi_umidita();
+  infoschermo();
+  if(POMPA){
+    digitalWrite(pin_pompa, HIGH);
     }
-   else {
-    digitalWrite(rele,HIGH);
-    statorele=LOW;
-    Serial.print("Umidità:");
-    Serial.print(umidita);
-    Serial.println("%");
-    stato_giorno();
-    Serial.println(luce);
-    Serial.println(GIORNO);
-    stato_pompa();
-    infoschermo();
-   }
-   delay(10000);
+    else{
+      digitalWrite(pin_pompa, LOW);
+      }
+  }  
+void leggi_umidita(){
+    umidita=1024-analogRead(sensore_uno);
+    umidita=map(umidita, 0, 1024, 0, 100);
+    if(umidita<50){
+      POMPA=true;
+      } else {
+        POMPA=false;
+        }
   }
-  // put your main code here, to run repeatedly:
-
 void infoschermo(){
   lcd.clear();
   lcd.setCursor(0,0);
@@ -79,26 +60,5 @@ void infoschermo(){
   else{
     lcd.print("OFF");
     }
-  }
-
-
-void stato_pompa(){
-  if(statorele==HIGH){
-    POMPA=true;
-    }
-    else{
-      POMPA=false;
-      }
-  
-
-}
-void stato_giorno(){
-  luce=analogRead(sensore_luce);
-  luce=map(luce, 0, 1024, 0, 100);
-  if(luce<=30){ //qui ho invertito maggiore e minore (vedi riga 17);
-    GIORNO=true;
-    }
-    else {
-      GIORNO=false;
-      }
+    delay(1000);
   }
